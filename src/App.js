7c59ad43adc3786401a9a8742dcad19f6a293c99
Elencarlos Soares Silva <elencarlos@uft.edu.br>
2021-09-24 13:38:36 -0300

    clean app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {pdf, PDFViewer} from '@react-pdf/renderer';
+import JSZip from "jszip";
+import {saveAs} from "file-saver";
+import MyDocument from "./Document"
+
+
+var zip = JSZip();
+
+const download = () => {
+    zip.generateAsync({type: "blob"}).then(function (blob) {
+        saveAs(blob, "projetos.zip");
+    });
+};
+
+export const generateZip = arr => {
+    console.log(arr)
+    if (arr.length) {
+        for (const element of arr) {
+            zip.file(`projeto/projeto_${element.name}.pdf`, element, {
+                binary: true
+            });
+        }
+
+        download();
+    }
+};
+
+
+const downloadContract = () => {
+
+    const buffer = pdf(<MyDocument />).toBlob();
+    buffer.then((b)=>{
+        const file = new File([b], "project1", {type: b.type, lastModified: 1534584790000});
+                generateZip([file])
+    })
+
+
+
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+        <PDFViewer>
+            <MyDocument />
+        </PDFViewer>
+        <button onClick={downloadContract}>Download</button>
+        </>
+    );
 }
 
 export default App;
